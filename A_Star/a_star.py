6693c823920853e87585e24a053ea9959c526cfc
Henry <HenrySky64@outlook.com>
2024-04-20 21:14:56 +0800

    improve a_star code

--- A_Star/a_star.py
+++ A_Star/a_star.py
@@ -1,6 +1,8 @@
+import os
+import heapq
 from Map.map import get_path
 import matplotlib.pyplot as plt
-import heapq
+from matplotlib.colors import LinearSegmentedColormap as LSC
 
 
 class Node:
@@ -25,7 +27,7 @@ def manhattan_dist(current, target):
     return abs(current.x - target.x) + abs(current.y - target.y)
 
 
-def a_star(map, start, goal, func=manhattan_dist):
+def a_star(map, start, goal, draw=False, func=manhattan_dist):
     assert (map[start[0]][start[1]] == 0 and map[goal[0]][goal[1]] == 0)
     # 获取起点和终点，创建 openset 和 closeset
     start_x, start_y = start
@@ -36,6 +38,15 @@ def a_star(map, start, goal, func=manhattan_dist):
     origin = Node(start_x, start_y)
     target = Node(goal_x, goal_y)
     heapq.heappush(open_set, origin)
+    # 开始绘图
+    if draw:
+        plt.ion()
+        plt.axis("off")
+        map[origin.x][origin.y] = 0.5
+        cmp = LSC.from_list("color", ['lightgrey','lightgreen', 'green','lightyellow', 'brown'])
+        plt.imshow(map, cmap=cmp)
+        plt.pause(0.5)
+    # 绘图过程
     while len(open_set) > 0:
         # openset 中弹出一个优先级最高的节点进行检验
         curr_node = heapq.heappop(open_set)
@@ -44,38 +55,57 @@ def a_star(map, start, goal, func=manhattan_dist):
             # 对最终结果进行溯源，找出起点到终点的路径
             while curr_node is not None:
                 result.append(curr_node)
+                # 开始绘图
+                if draw:
+                    plt.clf()
+                    plt.axis("off")
+                    map[curr_node.x][curr_node.y] = 0.5
+                    plt.imshow(map, cmap=cmp)
+                    plt.pause(0.1)
+                # 绘图过程
                 curr_node = curr_node.preNode
             result.reverse()
+            # 开始绘图
+            if draw:
+                plt.clf()
+                plt.axis("off")
+                plt.ioff()
+            # 绘图过程
             return True, result
         else:
-            close_set.add(curr_node)  # 排除当前节点
+            close_set.add((curr_node.x,curr_node.y))  # 排除当前节点
+            # 开始绘图
+            if draw:
+                plt.clf()
+                plt.axis("off")
+                map[curr_node.x][curr_node.y] = 0.8
+                plt.imshow(map, cmap=cmp)
+                plt.pause(0.1)
+            # 绘图过程
             # 搜寻下一个可能的节点，并加入 openset
             for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 new_x, new_y = curr_node.x + dx, curr_node.y + dy
                 if (0 <= new_x < len(map[0]) and 0 <= new_y < len(map)
-                        and map[new_x][new_y] != 1):
+                        and map[new_x][new_y] != 1) and (new_x, new_y) not in close_set:
                     new_node = Node(new_x, new_y)
                     new_node.distance = curr_node.distance + 1
                     new_node.heuristic = func(new_node, target)  # 调用启发函数，默认为曼哈顿距离
                     new_node.preNode = curr_node
                     heapq.heappush(open_set, new_node)
+                    # 开始绘图
+                    if draw:
+                        plt.clf()
+                        plt.axis("off")
+                        map[new_node.x][new_node.y] = 0.3
+                        plt.imshow(map, cmap=cmp)
+                        plt.pause(0.1)
+                    # 绘图过程
     return False, None
 
 
 def main():
     map = get_path("")
-    flag, path = a_star(map, (0, 0), (18, 18))
-    if flag:
-        # 开始绘图
-        plt.ion()
-        for node in path:
-            plt.clf()
-            plt.axis("off")
-            map[node.x][node.y] = 0.5
-            plt.imshow(map)
-            plt.pause(0.5)
-        plt.ioff()
-
+    flag, path = a_star(map, (0, 18), (18, 18), draw=True)
 
 if __name__ == '__main__':
     main()
